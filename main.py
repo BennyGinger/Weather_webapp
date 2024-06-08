ca479be86ced219924d2fb32ee9170e080a35e5f
Benoit Roux <benoit.roux@gmail.com>
2024-06-08 21:16:23 +0000

    day36

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title('Weather Forecast for the Next Days')
 
@@ -10,3 +11,13 @@ days = st.slider('Forecast Days:', 1, 5, 1, key='days', help='Select the number 
 option = st.selectbox('Select data to view:', ('Temperature', 'Sky'), key='option')
 
 st.subheader(f'{option} for the next {days} days in {place}')
+
+def get_data(days):
+    dates = ['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04', '2021-01-05', '2021-01-06']
+    temperature = [3,5,2,5,1,6]
+    return dates[:days+1], temperature[:days+1]
+
+dates,temperature = get_data(days)
+figure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature (C)'})
+                 
+st.plotly_chart(figure)
---
+++ side_project/happy.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import plotly.graph_objects as go
+import pandas as pd
+
+st.title("In Search for Happiness")
+data = pd.read_csv('happy.csv')
+
+x_axis = st.selectbox("Select data for the X-axix:", ('gdp', 'happiness', 'social_support', 'life_expectancy', 'freedom_to_make_life_choices', 'generosity', 'corruption'), key='x_axis',)
+
+y_axis = st.selectbox("Select data for the Y-axix:", ('gdp', 'happiness', 'social_support', 'life_expectancy', 'freedom_to_make_life_choices', 'generosity', 'corruption'), key='y_axis',)
+
+country = st.selectbox("Select a Country:", key='country', options=["None"]+list(data['country']))
+
+st.subheader(f"Comparing {x_axis} and {y_axis}")
+
+
+# fig = px.scatter(data, x=x_axis, y=y_axis, labels={'x': x_axis, 'y': y_axis}, hover_data=['country'])
+# st.plotly_chart(fig)
+fig = go.Figure()
+
+# Add scatter plot for all countries
+fig.add_trace(go.Scatter(x=data[data['country'] != country][x_axis], 
+                         y=data[data['country'] != country][y_axis], 
+                         mode='markers', 
+                         marker_color='blue',
+                         hovertemplate = f"<b>Country</b>: {data['country']}<br><b>{x_axis}</b>: %{x_axis}<br><b>{y_axis}</b>: %{y_axis}<extra></extra>"))
+
+# Add scatter plot for selected country
+if country != "None":
+    fig.add_trace(go.Scatter(x=data[data['country'] == country][x_axis], 
+                             y=data[data['country'] == country][y_axis], 
+                             mode='markers', 
+                             marker_color='red',
+                             hovertemplate = f"<b>Country</b>: {country}<br><b>{x_axis}</b>: %{x_axis}<br><b>{y_axis}</b>: %{y_axis}<extra></extra>"))
+
+fig.update_layout(xaxis_title=x_axis, yaxis_title=y_axis,showlegend=False)
+
+st.plotly_chart(fig)
