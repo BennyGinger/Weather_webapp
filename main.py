e65ecd4642299fa4f1a7deb3bef0da784382d3d3
Benoit Roux <benoit.roux@gmail.com>
2024-06-12 19:47:18 +0000

    final version

--- main.py
+++ main.py
@@ -16,19 +16,22 @@ st.subheader(f'{option} for the next {days} days in {place}')
 
 # Get data
 
+try:
+    data = get_data(place, days)
 
-data = get_data(place, days)
+    if option == 'Temperature':
+        dates = [x['dt_txt'] for x in data]
+        temperature = [round(x['main']['temp']/10,ndigits=1) for x in data]
+        figure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature (C)'})               
+        st.plotly_chart(figure)
 
-if option == 'Temperature':
-    dates = [x['dt_txt'] for x in data]
-    temperature = [round(x['main']['temp']/10,ndigits=1) for x in data]
-    figure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature (C)'})               
-    st.plotly_chart(figure)
+    else:
+        images_map = {'Clear':'/home/Weather_webapp/images/clear.png', 'Clouds':'/home/Weather_webapp/images/cloud.png','Rain':'/home/Weather_webapp/images/rain.png','Snow':'/home/Weather_webapp/images/snow.png'}
+        
+        sky_condition = [x['weather'][0]['main'] for x in data]
+        sky_condition = [images_map[x] for x in sky_condition]
+        dates = [x['dt_txt'] for x in data]
+        st.image(image=sky_condition, caption=dates, width=80, use_column_width=False)
 
-else:
-    images_map = {'Clear':'/home/Weather_webapp/images/clear.png', 'Clouds':'/home/Weather_webapp/images/cloud.png','Rain':'/home/Weather_webapp/images/rain.png','Snow':'/home/Weather_webapp/images/snow.png'}
-    
-    sky_condition = [x['weather'][0]['main'] for x in data]
-    sky_condition = [images_map[x] for x in sky_condition]
-    dates = [x['dt_txt'] for x in data]
-    st.image(image=sky_condition, caption=dates, width=80, use_column_width=False)
+except KeyError:
+    st.info(f"{place} does not exist. Please enter a valid place name")
